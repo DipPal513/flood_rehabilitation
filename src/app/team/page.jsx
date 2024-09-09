54b9c2d520ec31d9f9e191ebdcdbacc4702177b8
dippal513 <dip.pal.513@gmail.com>
2024-09-09 11:20:31 +0600

    website image added

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -8,30 +8,39 @@ const teamMembers = [
     role: 'Fundraising and Planning',
     phone: '+8801863333449',
     facebook: 'https://www.facebook.com/anid.hassan',
+    img:"/anid.jpg"
   },
   {
     name: 'Suprakash Bakali',
     role: 'Database and Fund Management',
     phone: '+8801521795475',
     facebook: 'https://www.facebook.com/bakali.me.in',
+    img:"/bakali.jpg"
+
   },
   {
     name: 'Riyadh Hossain',
     role: 'Publication Specialist',
     phone: '+8801824006979',
     facebook: 'https://www.facebook.com/riyadh.hossain.69',
+    img:"/riad.jpg"
+
   },
   {
     name: 'Shimanta Ranjan Chakma',
     role: 'Area survey support',
     phone: '+8801648336521',
     facebook: 'https://www.facebook.com/src.shimanta.35',
+    img:"/shimanta.jpg"
+
   },
   {
     name: 'Pritom Pal Dip',
     role: 'Website Design and maintenance',
     phone: '+8801625197575',
-    facebook:"https://www.facebook.com/dippal513"
+    facebook:"https://www.facebook.com/dippal513",
+    img:"/dip.jpg"
+
   },
   {
     name: 'Sadman Atik',
@@ -43,6 +52,7 @@ const teamMembers = [
     name: 'Rahat Rahman',
     role: 'External Affairs and support',
     phone: '+8801675137167',
+    img:"/rahat.jpeg",
     facebook: 'https://www.facebook.com/mohammad.rahat.39108',
   },
   {
@@ -50,12 +60,14 @@ const teamMembers = [
     role: 'Social Media management',
     phone: '+8801881337706',
     facebook: 'https://www.facebook.com/profile.php?id=100015569592333',
+    img:"/mamun.jpg"
   },
   {
     name: 'Shuvo Karmaker',
     role: 'Support',
     phone: '+8801745330624',
     facebook: 'https://www.facebook.com/suvo.suvokarmoker',
+    img:"/shuvo.jpg"
   },
   {
     name: 'Asif Shahriar Shammo',
@@ -68,6 +80,7 @@ const teamMembers = [
     role: 'Support',
     phone: '+8801864664247',
     facebook: 'https://www.facebook.com/profile.php?id=100074735670159',
+    img:"/masum.jpg"
   },
 ];
 const Team = () => {
@@ -83,17 +96,17 @@ const Team = () => {
                 <div className="relative flex flex-col items-center p-6">
                   <div className="">
                     <Image
-                      src="/avatar.png" // Replace with actual avatar image paths if available
+                      src={member.img || "/avatar.png"} // Replace with actual avatar image paths if available
                       alt={member.name}
                       width={120}
                       height={120}
-                      className="rounded-full border-4 border-red-500"
+                      className="rounded-full object-cover border-4 border-red-500 h-[120px]"
                     />
                   </div>
                   <div className="pt-16 pb-6 text-center">
                     <h3 className="text-2xl font-semibold text-gray-800 mb-2">{member.name}</h3>
                     <p className="text-lg text-gray-700 mb-1">{member.role}</p>
-                    <p className="text-sm text-gray-600 mb-4">{member.phone}</p>
+                    <Link href={"/call"} className="text-sm text-gray-600 mb-4 block">{member.phone}</Link>
                     {member.facebook && (
                       <Link href={member?.facebook || ""}
                         
